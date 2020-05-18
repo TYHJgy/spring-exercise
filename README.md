dca6f6dcf78ec58d8f37b0bde9e17226e92f1e0d
gaoyang <1094221167@qq.com>
2020-05-18 11:03:17 +0800

    v1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # spring-exercise
+spring boot 参考资料：
+https://docs.spring.io/spring-boot/docs/2.3.0.RELEASE/reference/html/index.html
+
+spring MVC 参考资料：
+https://docs.spring.io/spring/docs/5.2.6.RELEASE/spring-framework-reference/web.html#mvc
--- src/main/java/com/example/spring/exercise/controller/ApiUserController.java
+++ src/main/java/com/example/spring/exercise/controller/ApiUserController.java
@@ -1,3 +1,5 @@
+/*
+*/
 package com.example.spring.exercise.controller;
 
 import com.example.spring.exercise.domain.User;
@@ -8,6 +10,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 //@Configuration
