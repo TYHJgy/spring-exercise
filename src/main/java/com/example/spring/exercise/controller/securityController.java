e983c9a82b8cbbcc1017cbcc3d81628bcd9f69a6
Administrator <1094221167@qq.com>
2020-12-04 17:02:53 +0800

    2020-12-4 17:02:43

---
+++ src/main/java/com/example/spring/exercise/controller/securityController.java
@@ -0,0 +1,15 @@
+package com.example.spring.exercise.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@RestController
+@RequestMapping(path="/security")
+public class securityController{
+
+
+}
