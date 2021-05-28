b89f1ee3b18e6ccf5164d8937a0e11264d54e2aa
gy <gaoyang@richinfo.cn>
2021-05-28 09:24:58 +0800

    Revert "Revert "优化""
    This reverts commit 5495bbd42ed2e5873cbd13e002b97b18d6d95aef.

---
+++ src/main/java/com/example/spring/exercise/service/TestAutowiredServiceImpl1.java
@@ -0,0 +1,10 @@
+package com.example.spring.exercise.service;
+
+public class TestAutowiredServiceImpl1 implements TestAutowiredService{
+
+  @Override
+  public String test() {
+    return "TestAutowiredServiceImpl1";
+
+  }
+}
