2461a6537017fe99b5c8f3a2910cb57ff0e4ae59
gy <gaoyang@richinfo.cn>
2021-06-15 10:39:45 +0800

    ‘’

---
+++ src/main/java/com/example/spring/exercise/controller/OtherController.java
@@ -0,0 +1,26 @@
+package com.example.spring.exercise.controller;
+
+
+import static java.lang.System.*;
+
+import com.example.spring.exercise.domain.Task;
+import java.util.Date;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("Other")
+public class OtherController {
+
+
+  public void testBuilder() {
+    Task task = new Task.TaskBuilder(99, "紧急任务")
+        .type(1)
+        .status(0)
+        .finishDate(new Date())
+        .build();
+
+    out.println(task.toString());
+  }
+
+}
